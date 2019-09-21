fd1d73e154fea902b70a062147d4e9a83d30f327
Kirk <kirk.godber@gmail.com>
2019-09-21 13:52:34 +0100

    Version0.1

--- BowlingScores.java
+++ BowlingScores.java
@@ -1,10 +1,34 @@
-import java.io.Filewriter;
-import java.io.IOException
+import java.io.*;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject;
 
 public class BowlingScores 
 {
-	puboic static void main(String[] args)
+	public static void main(String[] args) throws Exception 
 	{
-		System.out.println("Hello World");		
+		Scanner scnFilePath = new Scanner(System.in);
+		System.out.println("Please enter the filepath of the scores.");
+		
+		String strFilepath = scnFilePath.next();
+		
+		//System.out.println(String.format("Filepath is %s", strFilepath));
+		
+		File fileScores = new File(strFilepath);
+		
+		if (!fileScores.exists())
+		{
+			System.out.println("File not found");
+			System.exit(0);
+		}
+		
+		BufferedReader brScoresFile = new BufferedReader(new FileReader(fileScores));
+		
+		String strFileContent;
+		while ((strFileContent = brScoresFile.readLine()) != null)
+			System.out.println(strFileContent);
+		
+		
 	}
 }
